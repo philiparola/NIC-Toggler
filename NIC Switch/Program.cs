15c9358ed0c21ca081021d242d8d9768db0b7b4c
Philip Arola <philiparola@hotmail.com>
2014-11-14 15:49:58 -0800

    Initial Working Copy

---
+++ NIC Switch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIC_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enable VPN or Home (the other will be disabled).");
+            string interfaceChoice = Console.ReadLine();
+            while (interfaceChoice != "VPN" && interfaceChoice != "Home")
+            {
+                Console.WriteLine("Type out the full adapter name.");
+                interfaceChoice = Console.ReadLine();
+            }
+
+            if (interfaceChoice == "VPN")
+            {
+                Enable(interfaceChoice);
+                string altNIC = "Home";
+                Disable(altNIC);
+            }
+
+            if (interfaceChoice == "Home")
+            {
+                Enable(interfaceChoice);
+                string altNIC = "VPN";
+                Disable(altNIC);
+            }
+
+        }
+
+        //The following is shamelessly ripped from http://stackoverflow.com/questions/172875/how-to-disable-enable-network-connection-in-c-sharp
+        static void Enable(string interfaceName)
+        {
+            System.Diagnostics.ProcessStartInfo psi =
+                   new System.Diagnostics.ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" enable");
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+
+        static void Disable(string interfaceName)
+        {
+            System.Diagnostics.ProcessStartInfo psi =
+                new System.Diagnostics.ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" disable");
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+    }
+}
